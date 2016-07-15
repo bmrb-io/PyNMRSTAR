9d635cd887a2f32100a9ecfd15e8e3a0b21d8218
Jon Wedell <wedell@bmrb.wisc.edu>
2016-07-15 12:03:34 -0500

    Implemented NMR-STAR specification following tokenizer. Potential bugs in it when handling malformed files, but it works on properly formed files.

--- c/parser.c
+++ c/parser.c
@@ -20,7 +20,6 @@ typedef struct {
 
 //void PyErr_SetString(void);
 
-
 void reset_parser(parser_data * parser){
     parser->source = NULL;
     free(parser->full_data);
@@ -34,40 +33,44 @@ void reset_parser(parser_data * parser){
 
 void print_parser_state(parser_data * parser){
     if (parser->source){
-        printf("parser(%s):\n", parser->source);
+        //printf("parser(%s):\n", parser->source);
     } else {
-        printf("parser(NULL)\n");
+        //printf("parser(NULL)\n");
         return;
     }
-    printf(" Pos: %lu/%lu\n", parser->index, parser->length);
-    printf(" Last delim: '%c'\n", parser->last_delineator);
-    printf(" Last token: '%s'\n\n", parser->token);
+    //printf(" Pos: %lu/%lu\n", parser->index, parser->length);
+    //printf(" Last delim: '%c'\n", parser->last_delineator);
+    //printf(" Last token: '%s'\n\n", parser->token);
 }
 
 /* Return the index of the first match of needle in haystack, or -1 */
-long index_handle(char * haystack, char * needle, long start_pos){
+long get_index(char * haystack, char * needle, long start_pos){
 
     haystack += sizeof(char) * start_pos;
     char * start = strstr(haystack, needle);
 
     // Word not found
+    //if (!start){
+    //    return -1;
+    //}
+    // Return the end if string not found
     if (!start){
-        return -1;
+        return strlen(haystack) - start_pos;
     }
 
-    // Calculate length into start is new word
+    // Calculate the length into start is the new word
     long diff = start - haystack;
     return diff;
 }
 
 void get_file(const char *fname, parser_data * parser){
-    printf("Parsing: %s\n", fname);
+    //printf("Parsing: %s\n", fname);
 
     // Open the file
     FILE *f = fopen(fname, "rb");
     if (!f){
         //PyErr_SetString(PyExc_IOError, "Could not open file.");
-        printf("Could not open file.");
+        //printf("Could not open file.");
         return;
     }
 
@@ -80,7 +83,7 @@ void get_file(const char *fname, parser_data * parser){
     char *string = malloc(fsize + 1);
     if (fread(string, fsize, 1, f) != 1){
         //PyErr_SetString(PyExc_IOError, "Short read of file.");
-        printf("File read error.");
+        //printf("File read error.");
         return;
     }
 
@@ -121,40 +124,154 @@ long get_next_whitespace(char * string, long start_pos){
     return pos;
 }
 
+/* Scan the index to the next non-whitespace char */
+void pass_whitespace(parser_data * parser){
+
+    //printf("Scan from %ld\n", parser->index);
+    while ((parser->index < parser->length) &&
+            (is_whitespace(parser->full_data[parser->index]))){
+        parser->index++;
+    }
+    //printf("Scan to %ld\n", parser->index);
+}
+
+/* Determines if we are done parsing. */
+bool check_finished(parser_data * parser){
+    if (parser->index == parser->length){
+        free(parser->token);
+        parser->token = NULL;
+        return true;
+    }
+    return false;
+}
+
+/* Returns a new token char * */
+char * update_token(parser_data * parser, long length){
+
+    if (parser->token != (void *)1){
+        free(parser->token);
+    }
+    parser->token = malloc(length+1);
+    memcpy(parser->token, &parser->full_data[parser->index], length);
+    parser->token[length] = '\0';
+
+    // Figure out what to set the last delineator as
+    if (parser->index == 0){
+        parser->last_delineator = 's';
+    } else {
+        char ld = parser->full_data[parser->index-1];
+        if ((ld == '\n') && (parser->index > 2) && (parser->full_data[parser->index-2] == ';')){
+            parser->last_delineator = ';';
+        } else if ((ld == '"') || (ld == '\'')){
+            parser->last_delineator = ld;
+        } else {
+            parser->last_delineator = ' ';
+        }
+    }
+
+    parser->index += length + 1;
+    return parser->token;
+}
+
 char * get_token(parser_data * parser){
 
-    // Free the existing token
-    free(parser->token);
+    // Reset the delineator
+    parser->last_delineator = '\0';
 
-    for (long x = parser->index; x < parser->length; x++){
-        // Keep track of the line number during tokenizing
-        if (parser->full_data[x] == '\n'){
-            parser->line_no++;
-        }
+    // Set up a tmp str pointer to use for searches
+    char * search;
 
-        // See if the character is whitespace
-        if (is_whitespace(parser->full_data[x])){
-            long token_size = x - parser->index;
-            parser->token = malloc(token_size+1);
-            memcpy(parser->token, &parser->full_data[parser->index], token_size);
-            parser->token[token_size] = '\0';
-            parser->index = x + 1;
-            parser->last_delineator = parser->full_data[x];
+    // Nothing left
+    if (parser->token == NULL){
+        //printf("get_token called with nothing left.\n");
+        return parser->token;
+    }
+
+    // We're at the end if the index is the length
+    if (parser->index >= parser->length){
+        //printf("just got to the end.\n");
+        parser->token = NULL;
+        return parser->token;
+    }
+
+    // Skip whitespace
+    pass_whitespace(parser);
+
+    // Stop if we are at the end
+    if (check_finished(parser)){
+        //printf("finished before seen token\n");
+        parser->token = NULL;
+        return parser->token;
+    }
+
+    // See if this is a comment - if so skip it
+    if (parser->full_data[parser->index] == '#'){
+        search = "\n";
+        //printf("Skipping comment of length: %ld\n", get_index(parser->full_data, search, parser->index));
+        parser->index += get_index(parser->full_data, search, parser->index);
+        return get_token(parser);
+    }
+
+    // See if this is a multiline comment
+    if ((parser->length - parser->index > 1) && (parser->full_data[parser->index] == ';') && (parser->full_data[parser->index+1] == '\n')){
+        //printf("Multiline\n");
+        search = "\n;";
+        long length = get_index(parser->full_data, search, parser->index);
+        parser->index += 2;
+        return update_token(parser, length-1);
+    }
+
+    // Handle values quoted with '
+    if (parser->full_data[parser->index] == '\''){
+        search = "'";
+        long end_quote = get_index(parser->full_data, search, parser->index + 1);
+
+        //TODO ERROR no terminating quote
+        //"Invalid file. Single quoted value was never "
+                                 //"terminated."
+        if (parser->index + end_quote == parser->length){
+            parser->index = parser->length;
+            parser->token = NULL;
             return parser->token;
         }
+
+        // Make sure we don't stop for quotes that are not followed by whitespace
+        while ((parser->index+end_quote+2 < parser->length) && (!is_whitespace(parser->full_data[parser->index+end_quote+2]))){
+            end_quote += get_index(parser->full_data, search, parser->index+end_quote+2) + 1;
+        }
+
+        // Move the index 1 to skip the '
+        parser->index++;
+        return update_token(parser, end_quote);
     }
 
-    // Last token
-    long token_size = parser->length - parser->index;
-    if (token_size == 0){
-        parser->token = NULL;
-    } else {
-        parser->token = malloc(token_size+1);
-        memcpy(parser->token, &parser->full_data[parser->index], token_size);
-        parser->token[token_size] = '\0';
+    // Handle values quoted with "
+    if (parser->full_data[parser->index] == '\"'){
+        search = "\"";
+        long end_quote = get_index(parser->full_data, search, parser->index + 1);
+
+        //TODO ERROR no terminating quote
+        //"Invalid file. Double quoted value was never "
+                                 //"terminated."
+        if (parser->index + end_quote == parser->length){
+            parser->index = parser->length;
+            parser->token = NULL;
+            return parser->token;
+        }
+
+        // Make sure we don't stop for quotes that are not followed by whitespace
+        while ((parser->index+end_quote+2 < parser->length) && (!is_whitespace(parser->full_data[parser->index+end_quote+2]))){
+            end_quote += get_index(parser->full_data, search, parser->index+end_quote+2) + 1;
+        }
+
+        // Move the index 1 to skip the "
+        parser->index++;
+        return update_token(parser, end_quote);
     }
-    parser->index = parser->length;
-    return parser->token;
+
+    // Nothing special. Just get the token
+    long end_pos = get_next_whitespace(parser->full_data, parser->index);
+    return update_token(parser, end_pos - parser->index);
 }
 
 // Get the current line number
@@ -171,16 +288,15 @@ long get_line_number(parser_data * parser){
 int main(int argc, char *argv[]){
 
     // Initialize the parser
-    parser_data parser = {NULL, NULL, NULL, 0, 0, 0, 0};
+    parser_data parser = {NULL, NULL, (void *)1, 0, 0, 0, 0};
 
     // Read the file
     get_file(argv[1], &parser);
 
     // Print the tokens
-    while(parser.index < parser.length){
-        get_token(&parser);
+    while(get_token(&parser) != NULL){
         //printf("Token (%lu): %s\n", get_line_number(&parser), parser.token);
-        //printf("Token: %s\n", parser.token);
+        //printf("Token (%c): %s\n", parser.last_delineator, parser.token);
     }
     reset_parser(&parser);
 }
