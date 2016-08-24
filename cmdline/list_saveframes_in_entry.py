46bab29755d171887011aee27e311dadca252b72
Jon Wedell <wedell@bmrb.wisc.edu>
2016-08-24 13:42:22 -0500

    Add some reference command line scripts for common NMR-STAR operations.

---
+++ cmdline/list_saveframes_in_entry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+sys.path.append("..")
+import bmrb
+
+if len(sys.argv) < 2:
+    raise ValueError("You must provide the file to read from as the first argument.")
+
+the_file = sys.argv[1]
+
+if not os.path.isfile(the_file):
+    raise IOError("The file you asked to read from does not exist.")
+    sys.exit(1)
+
+# Load the NMR-STAR file and print the list of saveframes in the format:
+# saveframe_name: saveframe_category
+for saveframe in bmrb.Entry.from_file(the_file):
+    print("%s: %s" % (saveframe.name, saveframe.category))
+
---
+++ cmdline/list_tags_in_entry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+sys.path.append("..")
+import bmrb
+
+if len(sys.argv) < 2:
+    raise ValueError("You must provide the file to read from as the first argument.")
+
+the_file = sys.argv[1]
+
+if not os.path.isfile(the_file):
+    raise IOError("The file you asked to read from does not exist.")
+    sys.exit(1)
+
+entry = bmrb.Entry.from_file(the_file)
+
+# Go through the saveframes and loops and print off the tags
+print("Entry %s" % entry.entry_id)
+for saveframe in entry:
+    print("  Saveframe %s:%s" % (saveframe.name, saveframe.category))
+    for tag in saveframe.tags:
+        print("    %s.%s" % (saveframe.tag_prefix, tag[0]))
+    for loop in saveframe:
+        print("    Loop %s" % loop.category)
+        for col in loop.columns:
+            print("      %s.%s" % (loop.category, col))
+
