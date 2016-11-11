ea55d1a912854c37482a0d3a5ef85281ba65fe82
Jon Wedell <wedell@bmrb.wisc.edu>
2016-11-11 14:51:00 -0600

    Bump to 2.2.2

--- do_release.sh
+++ do_release.sh
@@ -3,7 +3,7 @@
 while true; do
     read -p "Have you updated the version number in setup.py and pynmrstar/__init__.py? " yn
     case $yn in
-        [Yy]* ) pandoc -o README.rst README.md; python3 setup.py sdist upload --sign; break;;
+        [Yy]* ) cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; pandoc -o README.rst README.md; python3 setup.py sdist upload --sign; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
