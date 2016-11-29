5d42bdc819ea867ad466cf0473455b8203b025da
Jon Wedell <wedell@bmrb.wisc.edu>
2016-11-29 11:21:32 -0600

    Get no compilation check working properly.

--- do_release.sh
+++ do_release.sh
@@ -3,8 +3,18 @@
 while true; do
     read -p "Have you updated the version number in bmrb.py? " yn
     case $yn in
-        [Yy]* ) rm -rfv pynmrstar; mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; pandoc -o README.rst README.md; python3 setup.py sdist upload --sign; break;;
+        [Yy]* ) break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+while true; do
+    read -p "Do release or test? " rt
+    case $rt in
+        [Rr]* ) mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; touch pynmrstar/.nocompile; pandoc -o README.rst README.md; python3 setup.py sdist upload --sign; rm -rfv pynmrstar; break;;
+        [Tt]* ) mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; touch pynmrstar/.nocompile; pandoc -o README.rst README.md; python3 setup.py sdist; rm -rfv pynmrstar; break;;
+        * ) echo "Please answer r or t.";;
+    esac
+done
+
