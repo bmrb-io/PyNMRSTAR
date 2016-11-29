2c00d8853ac3853d1187052a7c9000e01fa0fe2e
Jon Wedell <wedell@bmrb.wisc.edu>
2016-11-29 11:32:02 -0600

    More cleanup.

--- do_release.sh
+++ do_release.sh
@@ -12,8 +12,8 @@ done
 while true; do
     read -p "Do release or test? " rt
     case $rt in
-        [Rr]* ) mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; touch pynmrstar/.nocompile; pandoc -o README.rst README.md; python3 setup.py sdist upload --sign; rm -rfv pynmrstar; break;;
-        [Tt]* ) mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; touch pynmrstar/.nocompile; pandoc -o README.rst README.md; python3 setup.py sdist; rm -rfv pynmrstar; break;;
+        [Rr]* ) mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; touch pynmrstar/.nocompile; pandoc -o README.rst README.md; python3 setup.py sdist upload --sign; rm -rfv pynmrstar pynmrstar.egg-info; break;;
+        [Tt]* ) mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; touch pynmrstar/.nocompile; pandoc -o README.rst README.md; python3 setup.py sdist; rm -rfv pynmrstar pynmrstar.egg-info; break;;
         * ) echo "Please answer r or t.";;
     esac
 done
