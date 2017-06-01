343df57bed8a6c5cdf13e3a58740e112742e9e5c
Jon Wedell <wedell@bmrb.wisc.edu>
2017-06-01 08:08:05 -0500

    Bump version number.

--- bmrb.py
+++ bmrb.py
@@ -209,7 +209,7 @@ _COMMENT_DICTIONARY = {}
 _API_URL = "http://webapi.bmrb.wisc.edu/v1"
 _SCHEMA_URL = 'http://svn.bmrb.wisc.edu/svn/nmr-star-dictionary/bmrb_only_files/adit_input/xlschem_ann.csv'
 _WHITESPACE = " \t\n\v"
-__version__ = "2.3.3"
+__version__ = "2.3.4"
 
 #############################################
 #             Module methods                #
