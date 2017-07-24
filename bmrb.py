1156e3116665da405ea360ad078d7d58396049fd
Jon Wedell <wedell@bmrb.wisc.edu>
2017-07-24 14:56:49 -0500

    API v2 change warrants minor version bump.

--- bmrb.py
+++ bmrb.py
@@ -209,7 +209,7 @@ _COMMENT_DICTIONARY = {}
 _API_URL = "http://webapi.bmrb.wisc.edu/v2"
 _SCHEMA_URL = 'http://svn.bmrb.wisc.edu/svn/nmr-star-dictionary/bmrb_only_files/adit_input/xlschem_ann.csv'
 _WHITESPACE = " \t\n\v"
-__version__ = "2.3.5"
+__version__ = "2.4"
 
 #############################################
 #             Module methods                #
