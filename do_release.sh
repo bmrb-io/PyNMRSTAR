4107e96806ce6b516b32482755077eb2eea5c0c6
Jon Wedell <wedell@bmrb.wisc.edu>
2018-07-26 16:09:23 -0500

    More minor changes.

--- do_release.sh
+++ do_release.sh
@@ -12,7 +12,7 @@ done
 while true; do
     read -p "Do release or test? " rt
     case $rt in
-        [Rr]* ) twine upload dist/*.tar.gz --sign; rm -rfv pynmrstar pynmrstar.egg-info; break;;
+        [Rr]* ) python3 -m twine upload dist/*.tar.gz --sign; rm -rfv pynmrstar pynmrstar.egg-info; break;;
         [Tt]* ) mkdir pynmrstar; cp bmrb.py pynmrstar/__init__.py; cp -rv reference_files pynmrstar; touch pynmrstar/.nocompile; pandoc -o README.rst README.md; python3 setup.py sdist; rm -rfv pynmrstar pynmrstar.egg-info; break;;
         * ) echo "Please answer r or t.";;
     esac
