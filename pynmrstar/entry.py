4366ad28b133f368ae608c5fa01f51da735366ed
Jon Wedell <wedell@bmrb.wisc.edu>
2018-08-17 13:44:36 -0500

    Update from_schema for Entry to set 'Original_NMR_STAR_version'.

--- pynmrstar/entry.py
+++ pynmrstar/entry.py
@@ -106,7 +106,9 @@ class Entry(object):
                     self.frame_list.append(saveframe.Saveframe.from_template(category, category,
                                                                              entry_id=self.entry_id,
                                                                              all_tags=kwargs['all_tags']))
-            self.get_saveframes_by_category('entry_information')[0]['NMR_STAR_version'] = schema.version
+            entry_saveframe = self.get_saveframes_by_category('entry_information')[0]
+            entry_saveframe['NMR_STAR_version'] = schema.version
+            entry_saveframe['Original_NMR_STAR_version'] = schema.version
             return
         else:
             # Initialize a blank entry
