c5b8e065666a048c3ae241e70d166cefeb847604
Jon Wedell <wedell@bmrb.wisc.edu>
2019-01-04 09:23:40 -0600

    Also import the parser in __init__.py

--- pynmrstar/__init__.py
+++ pynmrstar/__init__.py
@@ -2,9 +2,11 @@ from loop import Loop
 from saveframe import Saveframe
 from entry import Entry
 from schema import Schema
+from parser import Parser as _Parser
 from pynmrstar import *
 
 del loop
 del entry
 del saveframe
 del schema
+del parser
