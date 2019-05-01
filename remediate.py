10370ce5c48833df5c3b44a129493de1a60a792a
Jon Wedell <wedell@bmrb.wisc.edu>
2019-05-01 10:16:38 -0500

    Significantly speed up the remediation, and have it work on either/both the unicode issue and duplicate loop issue.

--- remediate.py
+++ remediate.py
@@ -12,6 +12,22 @@ patch_parser(pynmrstar)
 if not pynmrstar.cnmrstar:
     raise ValueError('The cnmrstar tokenizer must be compiled and available.')
 
+
+def get_comment(file_data_local):
+    """ Returns the file comment. Fixes the unicode character. """
+
+    # Get the comment at the beginning
+    comment_lines = []
+    pynmrstar.cnmrstar.load_string(file_data_local)
+    pynmrstar.cnmrstar.get_token_full()
+    token, line_number, delimiter = pynmrstar.cnmrstar.get_token_full()
+    while token and delimiter == '#':
+        comment_lines.append(token)
+        token, line_number, delimiter = pynmrstar.cnmrstar.get_token_full()
+    pynmrstar.cnmrstar.reset()
+    return "\n".join(comment_lines)
+
+
 for file_name in sys.argv[1:]:
 
     try:
@@ -21,22 +37,34 @@ for file_name in sys.argv[1:]:
             file_data = gzip.GzipFile(file_name, 'r').read()
         else:
             file_data = open(file_name, 'r').read()
-        pynmrstar.cnmrstar.load_string(file_data)
-
-        # Get the comment at the beginning
-        comment_lines = []
-        token = pynmrstar.cnmrstar.get_token_full()[0]
-        while token:
-            token, line_number, delimiter = pynmrstar.cnmrstar.get_token_full()
-            if delimiter == '#':
-                comment_lines.append(token)
-        comment_str = ("\n".join(comment_lines)).replace("–", '-')
-        pynmrstar.cnmrstar.reset()
 
         # Get the entry
         entry = pynmrstar.Entry.from_string(file_data)
-        del entry['constraint_statistics']
-        entry.rename_saveframe('global_Org_file_characteristics', 'constraint_statistics')
+
+        changes_made = False
+
+        # Fix the duplicate loop issue if needed
+        if 'global_Org_file_characteristics' in entry.frame_dict:
+            del entry['constraint_statistics']
+
+            # It's normally safer to us entry.rename_saveframe, but in this case we know there are no $references,
+            #  so it's much faster to reassign the name directly and skip checking all the other saveframes
+            rename_frame = entry.get_saveframe_by_name('global_Org_file_characteristics')
+            rename_frame.name = 'constraint_statistics'
+            rename_frame['sf_framecode'] = 'constraint_statistics'
+            changes_made = True
+
+        # Fix the unicode comment issue
+        comment_str = get_comment(file_data)
+
+        if "–" in comment_str:
+            comment_str = comment_str.replace("–", '-')
+            changes_made = True
+
+        if not changes_made:
+            continue
+        else:
+            print('Making changes...')
 
         sf_strings = []
         seen_saveframes = {}
@@ -49,8 +77,7 @@ for file_name in sys.argv[1:]:
 
         clean_string = "data_%s\n\n%s\n\n%s" % (entry.entry_id, comment_str, "\n".join(sf_strings))
     except Exception as err:
-        if 'global_Org_file_characteristics' not in str(err):
-            print("Warning! Something went wrong for file %s: %s" % (file_name, err))
+        print("Warning! Something went wrong for file %s: %s" % (file_name, err))
         continue
 
     if file_name.endswith('.gz'):
