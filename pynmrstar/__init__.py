49d9c960030d49793c3d3acfbc0aa48fdaa0b915
Jon Wedell <jonwedell@gmail.com>
2019-05-06 16:35:03 -0400

    Move utility functions to new .utils module to match v3, and add deprecation warnings to all methods that have moved. (But have them still work for now.)
    This will help prepare people for the upgrade to v3.

--- pynmrstar/__init__.py
+++ pynmrstar/__init__.py
@@ -87,6 +87,10 @@ from datetime import date
 from gzip import GzipFile
 
 # See if we have zlib
+from pynmrstar import _format_category, clean_value
+from pynmrstar.utils import _format_category, clean_value
+from . import utils
+
 try:
     import zlib
 except ImportError:
@@ -315,102 +319,9 @@ def validate(entry_to_validate, schema=None):
 
 
 def clean_value(value):
-    """Automatically quotes the value in the appropriate way. Don't
-    quote values you send to this method or they will show up in
-    another set of quotes as part of the actual data. E.g.:
+    """Deprecated. Please use utils.clean_value() instead."""
 
-    clean_value('"e. coli"') returns '\'"e. coli"\''
-
-    while
-
-    clean_value("e. coli") returns "'e. coli'"
-
-    This will automatically be called on all values when you use a str()
-    method (so don't call it before inserting values into tags or loops).
-
-    Be mindful of the value of STR_CONVERSION_DICT as it will effect the
-    way the value is converted to a string.
-
-    """
-
-    # Allow manual specification of conversions for booleans, Nones, etc.
-    if value in STR_CONVERSION_DICT:
-        if any(isinstance(value, type(x)) for x in STR_CONVERSION_DICT):
-            value = STR_CONVERSION_DICT[value]
-
-    # Use the fast code if it is available
-    if cnmrstar is not None:
-        # It's faster to assume we are working with a string and catch
-        #  errors than to check the instance for every object and convert
-        try:
-            return cnmrstar.clean_value(value)
-        except (ValueError, TypeError):
-            return cnmrstar.clean_value(str(value))
-
-    # Convert non-string types to string
-    if not isinstance(value, str):
-        value = str(value)
-
-    # If it is a STAR-format multiline comment already, we need to escape it
-    if "\n;" in value:
-        value = value.replace("\n", "\n   ")
-        if value[-1] != "\n":
-            value = value + "\n"
-        if value[0] != "\n":
-            value = "\n   " + value
-        return value
-
-    # If it's going on it's own line, don't touch it
-    if "\n" in value:
-        if value[-1] != "\n":
-            return value + "\n"
-        return value
-
-    if value == "":
-        raise ValueError("Empty strings are not allowed as values. "
-                         "Use a '.' or a '?' if needed.")
-
-    # If it has single and double quotes it will need to go on its
-    #  own line under certain conditions...
-    if '"' in value and "'" in value:
-        can_wrap_single = True
-        can_wrap_double = True
-
-        for pos, char in enumerate(value):
-            next_char = value[pos+1:pos+2]
-
-            if next_char != "" and next_char in _WHITESPACE:
-                if char == "'":
-                    can_wrap_single = False
-                if char == '"':
-                    can_wrap_double = False
-
-        if not can_wrap_single and not can_wrap_double:
-            return '%s\n' % value
-        elif can_wrap_single:
-            return "'%s'" % value
-        elif can_wrap_double:
-            return '"%s"' % value
-
-    # Check for special characters in a tag
-    if (any(x in value for x in " \t\v#") or
-            any(value.startswith(x) for x in
-                ["data_", "save_", "loop_", "stop_", "_"])):
-        # If there is a single quote wrap in double quotes
-        if "'" in value:
-            return '"%s"' % value
-        # Either there is a double quote or no quotes
-        else:
-            return "'%s'" % value
-
-    # Quote if necessary
-    if value[0] == "'":
-        return '"' + value + '"'
-    if value[0] == '"':
-        return "'" + value + "'"
-
-    # It's good to go
-    return value
+    return utils.clean_value(value)
 
 
 # Internal use only methods
@@ -424,23 +335,17 @@ def _json_serialize(obj):
 
 
 def _format_category(value):
-    """Adds a '_' to the front of a tag (if not present) and strips out
-    anything after a '.'"""
+    """ Deprecated. Please use utils.format_category() instead. """
 
-    if value:
-        if not value.startswith("_"):
-            value = "_" + value
-        if "." in value:
-            value = value[:value.index(".")]
-    return value
+    warnings.warn('This function has moved to utils.format_category().', DeprecationWarning)
+    return utils._format_category(value)
 
 
 def _format_tag(value):
-    """Strips anything before the '.'"""
+    """ Deprecated. Please use utils.format_tag() instead. """
 
-    if '.' in value:
-        value = value[value.index('.')+1:]
-    return value
+    warnings.warn('This function has moved to utils.format_tag().', DeprecationWarning)
+    return utils._format_tag(value)
 
 
 def _get_schema(passed_schema=None):
---
+++ pynmrstar/utils.py
@@ -0,0 +1,120 @@
+from pynmrstar import STR_CONVERSION_DICT, cnmrstar, _WHITESPACE
+
+
+def _format_tag(value):
+    """Strips anything before the '.'"""
+
+    if '.' in value:
+        value = value[value.index('.')+1:]
+    return value
+
+
+def _format_category(value):
+    """Adds a '_' to the front of a tag (if not present) and strips out
+    anything after a '.'"""
+
+    if value:
+        if not value.startswith("_"):
+            value = "_" + value
+        if "." in value:
+            value = value[:value.index(".")]
+    return value
+
+
+def clean_value(value):
+    """Automatically quotes the value in the appropriate way. Don't
+    quote values you send to this method or they will show up in
+    another set of quotes as part of the actual data. E.g.:
+
+    clean_value('"e. coli"') returns '\'"e. coli"\''
+
+    while
+
+    clean_value("e. coli") returns "'e. coli'"
+
+    This will automatically be called on all values when you use a str()
+    method (so don't call it before inserting values into tags or loops).
+
+    Be mindful of the value of STR_CONVERSION_DICT as it will effect the
+    way the value is converted to a string.
+
+    """
+
+    # Allow manual specification of conversions for booleans, Nones, etc.
+    if value in STR_CONVERSION_DICT:
+        if any(isinstance(value, type(x)) for x in STR_CONVERSION_DICT):
+            value = STR_CONVERSION_DICT[value]
+
+    # Use the fast code if it is available
+    if cnmrstar is not None:
+        # It's faster to assume we are working with a string and catch
+        #  errors than to check the instance for every object and convert
+        try:
+            return cnmrstar.clean_value(value)
+        except (ValueError, TypeError):
+            return cnmrstar.clean_value(str(value))
+
+    # Convert non-string types to string
+    if not isinstance(value, str):
+        value = str(value)
+
+    # If it is a STAR-format multiline comment already, we need to escape it
+    if "\n;" in value:
+        value = value.replace("\n", "\n   ")
+        if value[-1] != "\n":
+            value = value + "\n"
+        if value[0] != "\n":
+            value = "\n   " + value
+        return value
+
+    # If it's going on it's own line, don't touch it
+    if "\n" in value:
+        if value[-1] != "\n":
+            return value + "\n"
+        return value
+
+    if value == "":
+        raise ValueError("Empty strings are not allowed as values. "
+                         "Use a '.' or a '?' if needed.")
+
+    # If it has single and double quotes it will need to go on its
+    #  own line under certain conditions...
+    if '"' in value and "'" in value:
+        can_wrap_single = True
+        can_wrap_double = True
+
+        for pos, char in enumerate(value):
+            next_char = value[pos+1:pos+2]
+
+            if next_char != "" and next_char in _WHITESPACE:
+                if char == "'":
+                    can_wrap_single = False
+                if char == '"':
+                    can_wrap_double = False
+
+        if not can_wrap_single and not can_wrap_double:
+            return '%s\n' % value
+        elif can_wrap_single:
+            return "'%s'" % value
+        elif can_wrap_double:
+            return '"%s"' % value
+
+    # Check for special characters in a tag
+    if (any(x in value for x in " \t\v#") or
+            any(value.startswith(x) for x in
+                ["data_", "save_", "loop_", "stop_", "_"])):
+        # If there is a single quote wrap in double quotes
+        if "'" in value:
+            return '"%s"' % value
+        # Either there is a double quote or no quotes
+        else:
+            return "'%s'" % value
+
+    # Quote if necessary
+    if value[0] == "'":
+        return '"' + value + '"'
+    if value[0] == '"':
+        return "'" + value + "'"
+
+    # It's good to go
+    return value
