7aea7b641f9ffbd4cc383f3b6cb3882b42616c06
Jon Wedell <jonwedell@gmail.com>
2019-12-06 11:20:05 -0600

    Tweak __init__ imports

--- pynmrstar/__init__.py
+++ pynmrstar/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-"""This module provides Entry, Saveframe, and Loop objects. Use python's
-built in help function for documentation."""
+"""This module provides Entry, Saveframe, Loop, and Schema objects.
 
-import decimal
+It also provides some utility functions in pynmrstar.utils
 
-from . import definitions
-# noinspection PyCompatibility
-from . import exceptions
+Use python's built in help function for documentation."""
+
+import decimal as _decimal
+
 from . import utils
 from ._internal import __version__, _ensure_cnmrstar
 from .entry import Entry
@@ -17,7 +17,7 @@ from .saveframe import Saveframe
 from .schema import Schema
 
 # This makes sure that when decimals are printed a lower case "e" is used
-decimal.getcontext().capitals = 0
+_decimal.getcontext().capitals = 0
 # Make sure the cnmstar module is compiled
 _ensure_cnmrstar()
 del loop
@@ -25,6 +25,5 @@ del entry
 del saveframe
 del schema
 del parser
-del decimal
 
 __all__ = ['Loop', 'Saveframe', 'Entry', 'Schema', 'definitions', 'utils', '__version__', 'exceptions']
