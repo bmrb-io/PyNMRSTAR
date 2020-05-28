48f85715067e1c6d0985181ac37b75666126268d
Jon Wedell <jonwedell@gmail.com>
2020-05-28 17:41:33 -0500

    Improved build/test/release script.

--- release.sh
+++ release.sh
@@ -10,10 +10,11 @@ while true; do
 done
 
 while true; do
-    read -p "Do release or test? " rt
+    read -p "Do [b]uild, [t]est, or [r]elease? " rt
     case ${rt} in
-        [Rr]* ) python3 -m twine upload dist/*.tar.gz --sign; rm -rfv pynmrstar pynmrstar.egg-info; break;;
-        [Tt]* ) touch pynmrstar/.nocompile; python3 setup.py sdist; rm -rfv pynmrstar.egg-info; break;;
+        [Tt]* ) python3 -m twine upload --repository testpypi dist/*.tar.gz --sign; break;;
+        [Rr]* ) python3 -m twine upload dist/*.tar.gz --sign; break;;
+        [Bb]* ) touch pynmrstar/.nocompile; python3 setup.py sdist; rm -rfv pynmrstar.egg-info; break;;
         * ) echo "Please answer r or t.";;
     esac
 done
