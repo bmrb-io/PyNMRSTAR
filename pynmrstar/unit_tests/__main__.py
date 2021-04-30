da606e76032b2a80444fba1a51f0cad297a23b8a
jon@jonwedell.com <wedell@bmrb.wisc.edu>
2021-04-30 13:24:05 -0500

    Add some version information to the unit tests to verify the right python is being used.

--- pynmrstar/unit_tests/__main__.py
+++ pynmrstar/unit_tests/__main__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+import sys
+
 from pynmrstar.unit_tests import start_tests
 
 # Run unit tests if we are called directly
 if __name__ == '__main__':
+    print('Running tests...')
+    print(f'Python version: {sys.version}')
+    print(f'System platform: {sys.platform}')
     # Options, parse 'em
     start_tests()
