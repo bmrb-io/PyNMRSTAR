8a60f9c08c24acf5bd1747930e30009c9af62119
Jon Wedell <wedell@bmrb.wisc.edu>
2025-07-15 15:02:19 -0500

    Update sphinx to use the napoleon extension

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,11 @@ release = pynmrstar._internal.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
               'sphinx.ext.intersphinx',
               'sphinx.ext.imgconverter']
+
+napoleon_numpy_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
