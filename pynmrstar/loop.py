933498dd7d8c37cef93d74500c06fd9c6572e04a
Jon Wedell <wedell@bmrb.wisc.edu>
2026-01-12 15:24:13 -0600

    Improve typing of the loop iterator.

--- pynmrstar/loop.py
+++ pynmrstar/loop.py
@@ -141,7 +141,7 @@ class Loop(object):
         self.data = tmp_entry[0][0].data
         self.category = tmp_entry[0][0].category
 
-    def __iter__(self) -> list:
+    def __iter__(self) -> Generator[list[Any], Any, None]:
         """ Yields each of the rows contained within the loop. """
 
         for row in self.data:
